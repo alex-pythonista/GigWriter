a04d38b043d71a62e88d0d38e7e13a84a171b205
anikdev2016 <anikdev2016@gmail.com>
2023-03-16 21:12:33 +0600

    added the icon. it needs to be changed

--- background.js
+++ background.js
@@ -6,10 +6,10 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.storage.sync.get("enabled", function (data) {
     let isEnabled = data.enabled;
     chrome.storage.sync.set({ enabled: !isEnabled }, function () {
-      const iconPath = isEnabled ? "icon-disabled.png" : "icon.png";
+      const iconPath = isEnabled ? "./icon-disabled.png" : "./icon.png";
       chrome.browserAction.setIcon({ path: iconPath, tabId: tab.id });
       if (isEnabled) {
-        // Execute content script
+        // console.log(isEnabled);
         chrome.tabs.executeScript(tab.id, { file: "content.js" });
       } else {
         console.log("can't do it");
--- content.js
+++ content.js
@@ -1,1 +1,34 @@
 console.log("hello");
+// function to fetch the response from the API
+async function fetchToneResponse(tone) {
+  const response = await fetch(`https://example-api.com/tone?tone=${tone}`);
+  const data = await response.json();
+  return data;
+}
+
+// function to update the gig description
+function updateGigDescription(description) {
+  const gigDescriptionInput = document.querySelector("#description");
+  gigDescriptionInput.value = description;
+}
+
+// function to add the tone button
+function addToneButton(tone) {
+  const gigDescriptionDiv = document.querySelector(".description-wrapper");
+  const toneButton = document.createElement("button");
+  toneButton.innerText = tone;
+  toneButton.onclick = async () => {
+    const toneResponse = await fetchToneResponse(tone);
+    updateGigDescription(toneResponse.description);
+  };
+  gigDescriptionDiv.appendChild(toneButton);
+}
+
+// main function to add all tone buttons
+function addToneButtons() {
+  const tones = ["formal", "casual", "friendly", "informative"];
+  tones.forEach((tone) => addToneButton(tone));
+}
+
+// call the main function to add tone buttons
+addToneButtons();

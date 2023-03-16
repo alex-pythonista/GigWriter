f8b2455a75196e3c17d1c65b6a3a46a0ba59bd6d
anikdev2016 <anikdev2016@gmail.com>
2023-03-16 15:42:41 +0600

    completed background toggling and manifest

---
+++ background.js
@@ -0,0 +1,20 @@
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ enabled: true });
+});
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.storage.sync.get("enabled", function (data) {
+    let isEnabled = data.enabled;
+    chrome.storage.sync.set({ enabled: !isEnabled }, function () {
+      const iconPath = isEnabled ? "icon-disabled.png" : "icon.png";
+      chrome.browserAction.setIcon({ path: iconPath, tabId: tab.id });
+      if (isEnabled) {
+        // Execute content script
+        chrome.tabs.executeScript(tab.id, { file: "content.js" });
+      } else {
+        console.log("can't do it");
+      }
+    });
+    console.log(isEnabled);
+  });
+});

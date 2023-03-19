f28c961a7fa7f7c1f2a5a4c7bdd7866c8c17e78e
anikdev2016 <anikdev2016@gmail.com>
2023-03-19 20:22:35 +0600

    tested adding a span based on the toggled value

--- content.js
+++ content.js
@@ -5,11 +5,25 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
   console.log(changes.enabled);
   if (changes.enabled && changes.enabled.newValue !== undefined) {
     const isEnabled = changes.enabled.newValue;
+    const container = document.getElementById(":sy");
+
     if (isEnabled) {
       console.log("fetch here");
+      // Create the element
+      const span = document.createElement("span");
+      span.innerHTML = "hello world";
+
+      // Add it to the container
+      container.appendChild(span);
     } else {
       console.log("remove everything");
+      // Find the element and remove it
+      const span = container.querySelector("span");
+      if (span) {
+        container.removeChild(span);
+      }
     }
+
     // Send a message to the background script with the updated status
     chrome.runtime.sendMessage({ action: "updateStatus", enabled: isEnabled });
   }
